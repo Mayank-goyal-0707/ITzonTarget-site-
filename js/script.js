dfab8d351bd31296e68bd84909cb1a78edf97cf8
Mayank-goyal-0707 <mayankgoel199707@gmail.com>
2023-02-23 14:03:05 +0530

    code done for desktop homepage with slider

---
+++ js/script.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function(){
+    $('.customer-slider').slick({
+        dots:true,
+        infinite: false,
+        autoplay:false,
+        autoplayspeed:1500,
+        speed:300,
+        slideToShow:1,
+        SlideToScroll:1,
+        infinit:true,
+        arrows:false,
+    });
+});
